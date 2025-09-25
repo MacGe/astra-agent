57acad090242fca7ab2325cdef4c5611e4c34abe
Laevatain <laevatain.ef@gmail.com>
2025-09-25 10:18:43 +0800

    link const

--- core/plugin/link/consts/const.py
+++ core/plugin/link/consts/const.py
@@ -8,16 +8,16 @@ and used by other modules via attribute access (e.g., const.service_name_key).
 import os
 
 # common
-# pylint: disable=unused-import  # These imports are used via module attribute access
+# These imports are used via module attribute access
 from plugin.link.consts.keys.xc_utils_keys import (
     SERVICE_NAME_KEY,
+    SERVICE_SUB_KEY,
+    SERVICE_LOCATION_KEY,
     OTLP_ENABLE_KEY,
     OTLP_DC_KEY,
     OTLP_SERVICE_NAME_KEY,
-    SERVICE_SUB_KEY,
-    SERVICE_LOCATION_KEY,
-    SERVICE_PORT_KEY,
     KAFKA_TOPIC_KEY,
+    # SERVICE_PORT_KEY,
 )
 
 # mysql
@@ -58,18 +58,6 @@ from plugin.link.consts.keys.uvicorn_keys import (
     SERVICE_PORT_KEY,
 )
 
-# xingchen utils keys
-# pylint: disable=unused-import  # These imports are used via module attribute access
-from plugin.link.consts.keys.xc_utils_keys import (
-    OTLP_DC_KEY,
-    OTLP_ENABLE_KEY,
-    OTLP_SERVICE_NAME_KEY,
-    SERVICE_LOCATION_KEY,
-    SERVICE_NAME_KEY,
-    SERVICE_SUB_KEY,
-    # SERVICE_PORT_KEY,
-)
-
 # Environment variables
 Env = os.getenv(ENVIRONMENT_KEY)
 ENV_PRODUCTION = "production"
@@ -90,27 +78,25 @@ DEF_VER = "V1.0"
 DEF_DEL = 0
 
 __all__ = [
-    "DATACENTER_ID_KEY",
-    "DEFAULT_APPID_KEY",
-    "DOMAIN_BLACK_LIST_KEY",
-    "HTTP_AUTH_AWAU_API_KEY_KEY",
-    "HTTP_AUTH_AWAU_API_SECRET_KEY",
-    "HTTP_AUTH_AWAU_APP_ID_KEY",
-    "HTTP_AUTH_QU_APP_ID_KEY",
-    "HTTP_AUTH_QU_APP_KEY_KEY",
-    "IP_BLACK_LIST_KEY",
-    "OFFICIAL_TOOL_KEY",
-    "SEGMENT_BLACK_LIST_KEY",
-    "THIRD_TOOL_KEY",
-    "WORKER_ID_KEY",
+    # xc_utils_keys
+    "SERVICE_NAME_KEY",
+    "SERVICE_SUB_KEY",
+    "SERVICE_LOCATION_KEY",
+    "OTLP_ENABLE_KEY",
+    "OTLP_DC_KEY",
+    "OTLP_SERVICE_NAME_KEY",
+    "KAFKA_TOPIC_KEY",
+    # mysql_keys
     "MYSQL_DB_KEY",
     "MYSQL_HOST_KEY",
     "MYSQL_PASSWORD_KEY",
     "MYSQL_PORT_KEY",
     "MYSQL_USER_KEY",
+    # redis_keys
     "REDIS_ADDR_KEY",
     "REDIS_CLUSTER_ADDR_KEY",
     "REDIS_PASSWORD_KEY",
+    # spark_keys
     "CONFIG_FILE_KEY",
     "ENVIRONMENT_KEY",
     "LOG_LEVEL_KEY",
@@ -121,11 +107,6 @@ __all__ = [
     "POLARIS_USERNAME_KEY",
     "PROJECT_NAME_KEY",
     "VERSION_KEY",
+    # uvicorn_keys
     "SERVICE_PORT_KEY",
-    "OTLP_DC_KEY",
-    "OTLP_ENABLE_KEY",
-    "OTLP_SERVICE_NAME_KEY",
-    "SERVICE_LOCATION_KEY",
-    "SERVICE_NAME_KEY",
-    "SERVICE_SUB_KEY",
 ]
